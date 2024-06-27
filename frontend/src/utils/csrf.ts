93865ca4fc5d4f118a751a47e143afecff3a52ec
Charles Oller <charlesrello@gmail.com>
2024-06-27 19:28:04 -0400

    FE and BE working + deployed

--- frontend/src/utils/csrf.ts
+++ frontend/src/utils/csrf.ts
@@ -10,7 +10,6 @@ export async function csrfFetch(url, options = {}) {
   // if the options.method is not 'GET', then set the "Content-Type" header to
   // "application/json", and set the "XSRF-TOKEN" header to the value of the
   // "XSRF-TOKEN" cookie
-  console.log("fetching")
   if (options.method.toUpperCase() !== 'GET') {
     options.headers['Content-Type'] =
     options.headers['Content-Type'] || 'application/json';
@@ -18,7 +17,6 @@ export async function csrfFetch(url, options = {}) {
   }
 
   if (env === "production") {
-    console.log("this is production")
     try {
       const res = await fetch("https://dimension-1.onrender.com" + url, options);
       return res;
