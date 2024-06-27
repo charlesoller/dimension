9532d84be76d48fd46ed6eb4eaa4cef17dcbcf84
Charles Oller <charlesrello@gmail.com>
2024-06-27 18:48:28 -0400

    Altered index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -49,26 +49,26 @@ app.use(
 
 // Static routes
 // Serve React build files in production
-// if (process.env.NODE_ENV === 'production') {
-//   // Serve the frontend's index.html file at the root route
-//   app.get('/', (req, res) => {
-//     res.cookie('XSRF-TOKEN', req.csrfToken());
-//     res.sendFile(
-//       path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
-//     );
-//   });
+if (process.env.NODE_ENV === 'production') {
+  // Serve the frontend's index.html file at the root route
+  // app.get('/', (req, res) => {
+  //   res.cookie('XSRF-TOKEN', req.csrfToken());
+  //   res.sendFile(
+  //     path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+  //   );
+  // });
 
-//   // Serve the static assets in the frontend's build folder
-//   app.use(express.static(path.resolve("../frontend/dist")));
+  // Serve the static assets in the frontend's build folder
+  app.use(express.static(path.resolve("../frontend/dist")));
 
-//   // Serve the frontend's index.html file at all other routes NOT starting with /api
-//   app.get(/^(?!\/?api).*/, (req, res) => {
-//     res.cookie('XSRF-TOKEN', req.csrfToken());
-//     res.sendFile(
-//       path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
-//     );
-//   });
-// }
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  // app.get(/^(?!\/?api).*/, (req, res) => {
+  //   res.cookie('XSRF-TOKEN', req.csrfToken());
+  //   res.sendFile(
+  //     path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+  //   );
+  // });
+}
 
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
