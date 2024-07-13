7214bb870aa5f09ad19015ace77550e425bbea1b
charlesoller <charlesrello@gmail.com>
2024-07-13 13:41:08 -0400

    CSRF fix

--- frontend/src/utils/csrf.ts
+++ frontend/src/utils/csrf.ts
@@ -40,7 +40,6 @@ export async function csrfFetch(url, options = {}) {
 
 }
 
-// call this to get the "XSRF-TOKEN" cookie, should only be used in development
 export function restoreCSRF() {
-    return csrfFetch('/api/csrf/restore');
+  return csrfFetch('/api/csrf/restore');
 }
