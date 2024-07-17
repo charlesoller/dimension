2a8293bbf323f6cbafa1b6ad3dc16fa210eee4c3
charlesoller <charlesrello@gmail.com>
2024-07-17 19:09:06 -0400

    Added loading state for server spin down

--- frontend/src/routes/Landing/Landing.tsx
+++ frontend/src/routes/Landing/Landing.tsx
@@ -7,21 +7,29 @@ import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import { IPost } from "../../utils/types";
 import { useEffect } from "react";
 import { loadAllPostsThunk } from "../../store/posts";
+import { RingLoader } from "react-spinners";
 // Types
 
-export default function Landing(){
+export default function Landing() {
   const dispatch = useDispatch();
   // shallowEqual is needed below to avoid infinite rerenders, since visiblePosts is derived from posts, we don't want
   // posts to change if visiblePosts does
-  const posts = useSelector((state: any)=> Object.values(state.posts), shallowEqual);
+  const posts = useSelector((state: any) => Object.values(state.posts), shallowEqual);
 
   useEffect(() => {
-      dispatch(loadAllPostsThunk() as any)
+    dispatch(loadAllPostsThunk() as any)
   }, [])
 
   return (
     <main className={styles.body}>
-      <Feed posts={posts}/>
+      {posts.length ? (
+        <Feed posts={posts} />
+      ) : (
+        <div className={styles.loadingState}>
+          <RingLoader color="#E4E4E7"/>
+          <p>Please wait while the server spins up. This may take a moment.</p>
+        </div>
+      )}
     </main>
   )
 }
